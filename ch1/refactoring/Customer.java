c917e9b2bdbccbdbc24a0105d48be0c1e57d8d20
taikomegane <taiko.megane.gobou@gmail.com>
2017-08-20 18:49:10 +0900

    commit

---
+++ ch1/refactoring/Customer.java
@@ -0,0 +1,74 @@
+package refactoring;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class Customer {
+
+	private String _name;
+	private Vector _rentals = new Vector();
+
+	public Customer(String name){
+		_name = name;
+	}
+
+	public void addRental(Rental arg){
+		_rentals.addElement(arg);
+	}
+
+	public String getName(){
+		return _name;
+	}
+
+	public String statement(){
+
+		double totalAmount = 0;
+		int frequentRenterPoints = 0;
+		Enumeration rentals = _rentals.elements();
+		String result = "Rental Record for " + getName() + "\n";
+		while (rentals.hasMoreElements()){
+			// double thisAmount = 0;
+			Rental each = (Rental) rentals.nextElement();
+
+			// thisAmount = amountFor(each);
+
+			/*
+			// 一行ごとに金額を計算
+			switch (each.getMovie().getPriceCode()){
+			case Movie.REGULAR:
+				thisAmount += 2;
+				if (each.getDaysRented() > 2)
+					thisAmount += (each.getDaysRented() - 2) * 1.5;
+				break;
+			case Movie.NEW_RELEASE:
+				thisAmount += each.getDaysRented() * 3;
+				break;
+			case Movie.CHILDRENS:
+				thisAmount += 1.5;
+				if (each.getDaysRented() > 3)
+					thisAmount += (each.getDaysRented() - 3) * 1.5;
+				break;
+			}
+			*/
+
+			// レンタルポイントを加算
+			frequentRenterPoints ++;
+
+			// 新作を２日委譲借りた場合はボーナスポイント
+			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) frequentRenterPoints ++;
+
+			// この貸出に関する数値の表示
+			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
+			totalAmount += each.getCharge();
+		}
+
+		// フッタ部分の追加
+		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+		return result;
+	}
+
+	public double amountFor(Rental aRental){
+		return aRental.getCharge();
+	}
+}
